9b5556e967275e99fdd43141a6da7d9dee88350c
Liang <gaoxiaoliangz@users.noreply.github.com>
2017-11-15 22:47:18 +0800

    react & css ready

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,17 +1,22 @@
 const getBaseConfig = require('../lib/get-base-config')
 
-const config = getBaseConfig({
-  features: {
-    react: {
-      reactyes: 1
-    },
-    babel: {
-      reactyes: 0,
-      other: 2
-    },
-    sass: true,
-    postcss: true
+const config = getBaseConfig(
+  {
+    features: {
+      polyfill: true,
+      react: true,
+      babel: {
+        reactyes: 0,
+        other: 2
+      },
+      sass: true,
+      // postcss: true
+    }
+  }, {
+    entry: {
+      app: ['babel-polyfill'],
+    }
   }
-})
+)
 
 // console.log(config)
