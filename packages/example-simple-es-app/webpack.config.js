862edec0a889312cb3ca1b6cb6afa31e6160d87a
Liang <gaoxiaoliangz@gmail.com>
2019-05-10 13:08:06 +0800

    setup example

--- packages/example-simple-es-app/webpack.config.js
+++ packages/example-simple-es-app/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
 module.exports = {
-  entry: path.resolve('./src/index.js'),
-  output: {
-    libraryTarget: 'umd',
-  },
+  // entry: path.resolve('./src/index.js'),
+  // output: {
+  //   libraryTarget: 'umd',
+  // },
 }
