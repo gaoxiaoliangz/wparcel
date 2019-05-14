923d95117ff102713d65c4be18ca1e9572760ffb
gaoxiaoliangz <gaoxiaoliangz@gmail.com>
2019-05-15 00:16:37 +0800

    remove files

--- packages/cli/src/helpers/html.spec.ts
+++
@@ -1,7 +0,0 @@
-import { parseHtml } from './html'
-
-describe('parseHtml', () => {
-  test('#1', () => {
-    expect(parseHtml('abc')).toEqual({ ok: true })
-  })
-})
--- packages/cli/src/helpers/html.ts
+++
@@ -1,3 +0,0 @@
-export const parseHtml = (htmlString: string) => {
-  return { ok: true }
-}
