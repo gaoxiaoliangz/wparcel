ea22f3db136107cc8b603f4ba6c7a318f32df00e
Liang <gaoxiaoliangz@gmail.com>
2019-05-14 14:19:27 +0800

    remove old code

--- packages/cli/src/utils/print.ts
+++ packages/cli/src/utils/print.ts
@@ -7,26 +7,6 @@ const makePrintFn = (colorFn?) => (...args) => {
   return console.log(...args)
 }
 
-// const print2 = {
-//   log(...args) {
-//     console.info.apply(null, Array.from(args))
-//   },
-//   info(...args) {
-//     console.info.apply(null, Array.from(args).map(arg => chalk.cyan(arg)))
-//   },
-//   warn(...args) {
-//     console.warn.apply(null, Array.from(args).map(arg => chalk.yellow(arg)))
-//   },
-//   error(...args) {
-//     if (args[0] instanceof Error) {
-//       const err = args[0]
-//       console.error(chalk.red(err.stack))
-//       return
-//     }
-//     console.error.apply(null, Array.from(args).map(arg => chalk.red(arg)))
-//   },
-// }
-
 const print = {
   log: makePrintFn(),
   info: makePrintFn(chalk.cyan),
