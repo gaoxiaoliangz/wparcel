21fbaa54f4e8f30a253ff4a64b78f49938f1d397
gaoxiaoliangz <gaoxiaoliangz@gmail.com>
2019-05-15 23:39:46 +0800

    add ts app

---
+++ packages/example-ts-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import cat from './cat.gif'
+
+const App = () => {
+  return (
+    <div>
+      <img src={cat} alt="cat" />
+      <p>react app works</p>
+    </div>
+  )
+}
+
+export default App
