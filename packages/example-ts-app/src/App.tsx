8ec9a3ff7747b433e97f59f9315b48f9982f410d
gaoxiaoliangz <gaoxiaoliangz@gmail.com>
2019-05-15 23:52:46 +0800

    add types

--- packages/example-ts-app/src/App.tsx
+++ packages/example-ts-app/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import cat from './cat.gif'
 
-const App = () => {
+interface AppProps {
+  title: string
+}
+
+const App = ({ title }: AppProps) => {
   return (
     <div>
       <img src={cat} alt="cat" />
-      <p>react app works</p>
+      <p>{title} works</p>
     </div>
   )
 }
